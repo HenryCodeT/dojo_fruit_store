66688f55c05f7888702b723f9aa15ec47218dbdc
HenryCodeT <120686@unsaac.edu.pe>
2022-01-21 15:38:47 -0500

    get form to display on checkout.html

--- server.py
+++ server.py
@@ -1,14 +1,27 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)  
 
+all_checkouts=[]
+
 @app.route('/')         
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
+@app.route('/checkout', methods=['POST','GET'])         
 def checkout():
-    print(request.form)
-    return render_template("checkout.html")
+    # print(request.form)
+    checkout_info={
+        'first_name':request.form['first_name'],
+        'last_name':request.form['last_name'],
+        'student_id':request.form['student_id'],
+        'strawberry':request.form['strawberry'],
+        'raspberry':request.form['raspberry'],
+        'apple':request.form['apple'],
+    }
+    all_checkouts.append(checkout_info)
+    count = int(request.form['strawberry'])+int(request.form['raspberry'])+int(request.form['apple'])
+    print(f"Cobrando a {checkout_info['first_name']} por {count} frutas")
+    return render_template("checkout.html",  checkout_info = checkout_info)
 
 @app.route('/fruits')         
 def fruits():
